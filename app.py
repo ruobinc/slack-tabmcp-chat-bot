84713c28030f71c41689724a09b35f92d6b970f7
ruobinc <ruobin.chang@gmail.com>
2026-02-23 10:11:52 +0900

    app.pyをモジュール構成に更新

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from handlers.mention import handle_mention
+from handlers.dm import handle_dm
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
+# チャンネル/スレッドでの@メンション
+app.event("app_mention")(handle_mention)
 
-@app.event("app_mention")
-def handle_mention(say):
-    say("こんにちは")
-
+# DMメッセージ
+app.event("message")(handle_dm)
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
